6464b023e0b781dd66110d415e2c4a8d96a6a407
AliceCryptoStudy <115705453+AliceCryptoStudy@users.noreply.github.com>
2025-09-18 20:00:07 +0800

    Updated package.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./main.scss";
+import { configure } from "mobx";
+import ReactDOM from "react-dom/client";
+import { initLang } from "./locale";
+import { App } from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+window.onload = async () => {
+  await initLang();
+  configure({
+    enforceActions: "never",
+    useProxies: "ifavailable",
+  });
+
+  const root = document.getElementById("root")!;
+  ReactDOM.createRoot(root).render(<App />);
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+/// <reference types="vitest" />
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   server: {
     port: 3000,
-    host: '0.0.0.0'
+    host: "0.0.0.0"
   },
-})
+  preview: {
+    port: 3001,
+    host: "0.0.0.0",
+  },
+  test: {
+    include: ["tests/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+  },
+});
